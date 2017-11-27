e1ce67689d04d53708ca0f8e9952a24090319a88
Amir Ali Jiwani <amirali.jiwani89@gmail.com>
2017-11-27 23:22:07 +0800

    Adds sample routes as well as body parser usage to the app.

--- app/routes/car_ads_routes.js
+++ app/routes/car_ads_routes.js
@@ -1,3 +1,15 @@
 module.exports = function (app, db) {
 
+    // Create Ad
+    app.post("/car_ads", (req, res) => {
+        // Here we create the ad
+        console.log(req.body);
+        res.send("hello");
+    });
+
+    // View Ad
+
+    // Modify Ad
+
+    // Delete Ad
 };
