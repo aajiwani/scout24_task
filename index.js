fde5499df130c7f7fffc4d6471143ce77647b0e9
Amir Ali Jiwani <amirali.jiwani89@gmail.com>
2017-11-30 16:46:23 +0800

    Adds ability to handle CORS.

--- index.js
+++ index.js
@@ -6,7 +6,12 @@ const app = express();
 
 const port = 8000;
 
-// app.use(bodyParser.urlencoded({ extended: true }));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(bodyParser.json());
 
 mongoose.Promise = global.Promise;
